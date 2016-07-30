400b621c0f48a9ef4d979d5f2ee9ffa11e182f53
Mike Conrad <mkcnrd@gmail.com>
2016-07-30 12:19:23 -0400

    Update lunchmenu.sh

--- lunchmenu.sh
+++ lunchmenu.sh
@@ -3,12 +3,17 @@
 #  lunch/Dinner menu followed by the specials menu.  Currently for breakfast we are simply displaying a still slide using
 #  FEH.  This is placed into a cronjob.  We want to make sure that we kill any of these existing processes.  
 
+# 07/30/16 - Changed the way we set up the socket variable, the previous method was too broad, changed it so that we always know the
+# socket of the currently running version of uzbl.  This script now works together with a php script that can be called via curl
+# allowing us to essentially build a remote control app for UZBl
+
 #!/bin/bash
-socket=/tmp/uzbl_socket_*
+#socket=/tmp/uzbl_socket_* #Old Method
+socket=/tmp/uzbl_socket_`ps -aux | pgrep uzbl`
+
 blackimage=$( hexdump blank.png | wc | awk '{print$1}')
-url=http://192.168.10.168/index.php
+url=http://m58cafe.calvarychatt.com/index.php
 DISPLAY=:0
-EXPORT $DISPLAY
 
 echo "Turning off Power Save"
 
